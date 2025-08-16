f3ac4917350ff7f6c38d905b472bb5b8adf51958
Jacob Erasmus <j.g.erasmus@outlook.com>
2025-08-16 18:14:50 +0200

    added logisitc growth function method

--- src/GrowthFunction.java
+++ src/GrowthFunction.java
@@ -1,24 +1,32 @@
 public class GrowthFunction {
-    private float maxSize;
-    private float growthRate;
+    private float maxSize;    //what is this for?
+    private float growthRate;    //what is this for?
     
     public GrowthFunction() {
-        this.maxSize = 1.0f;
-        this.growthRate = 0.1f;
+        this.maxSize = 1.0f;    //what is this for?
+        this.growthRate = 0.1f;     //what is this for?
     }
     
-    public GrowthFunction(float maxSize, float growthRate) {
+    public GrowthFunction(float maxSize, float growthRate) {    //confused as why these parameters need?
         this.maxSize = maxSize;
         this.growthRate = growthRate;
     }
     
-    public float calculateSize(int age) {
-        // Method stub
-        return 0.0f;
+    public float calculateSize(Plant plant, boolean open) {
+        float maxHeight;
+        if (open){
+            maxHeight = plant.getMaxHeightOpen();
+        }else{
+            maxHeight = plant.getMaxHeightClosed(); 
+        }
+        // h = ((2 / 1 + e^((t/tm)*q)) - 1) * mh
+        // where t is the current tree age in years, tm is the maximum achievable tree age in years,
+        // mh is maximum achieve height in metres.
+        double height = ((2/(1 + Math.pow(Math.E, (plant.getAge()/plant.getLifeSpan())*plant.getQ())) - 1) * maxHeight);
+        return (float)height;
     }
     
-    public int getMaxAge() {
-        // Method stub
+    public int getMaxAge() {    //what is this method for?
         return 0;
     }
 }
