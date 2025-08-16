8369fecc51cf67c17f0a6e3e9419cf75b50c1f54
Jacob Erasmus <j.g.erasmus@outlook.com>
2025-08-16 16:12:29 +0200

    Reslove merge conflicts

--- src/Vector.java
+++ src/Vector.java
@@ -20,7 +20,7 @@ public class Vector {
         }
     }
 
-    //Find Dot Product (the cosine of the angle)
+    //Find Dot Product
     public double dot(Vector b){
         return this.x * b.x + this.y * b.y + this.z * b.z;
     }
