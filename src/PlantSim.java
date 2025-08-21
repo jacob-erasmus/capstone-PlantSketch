f236b192ed67164a3ff474ef6b12c088497a002a
ellathorburn <99ellathor@gmail.com>
2025-08-21 16:33:29 +0200

    adding forest visualizer

--- src/PlantSim.java
+++ src/PlantSim.java
@@ -222,6 +222,8 @@ public class PlantSim {
 
         // export to 2D renderer
         ForestVisualizer.showForest(forest, dimX, dimY, 3);
+        ForestOnTerrainVisualizer.showForestOnTerrain(forest, fileManager.getElevationGrid());
+
 
         
     }
