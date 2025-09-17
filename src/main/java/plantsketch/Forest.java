3f17d97c541812417e1c3c74af987a883b800ff9
ellathorburn <99ellathor@gmail.com>
2025-09-17 20:22:58 +0200

    refactoring the forest class to be more dynamic and efficient

--- src/main/java/plantsketch/Forest.java
+++ src/main/java/plantsketch/Forest.java
@@ -23,31 +23,26 @@ public class Forest {
     }
 
     // for local changes
-    public void addPlant(Plant plant) {
-        for (int i = 0; i < 7; i++) {
-            if (speciesMap.get(i).getSpecies() == plant.getSpecies()) {
-                speciesMap.get(i).setPlantAt(plant);
-                break;
-            }
-        }
-    }
+public void addPlant(Plant plant) {
+    speciesMap.stream()
+        .filter(sm -> sm.getSpecies() == plant.getSpecies())
+        .findFirst()
+        .ifPresent(sm -> sm.setPlantAt(plant));
+}
 
     // for local changes
-    public void removePlant(Plant plant) {
-        for (int i = 0; i < 7; i++) {
-            if (speciesMap.get(i).getSpecies() == plant.getSpecies()) {
-                speciesMap.get(i).setPlantAt(plant);
-                break;
-            }
-        }
-    }
+public void removePlant(Plant plant) {
+    speciesMap.stream()
+        .filter(sm -> sm.getSpecies() == plant.getSpecies())
+        .findFirst()
+        .ifPresent(sm -> sm.removePlantAt(plant.getX(), plant.getY()));
+}
 
-    public ArrayList<Plant> getAllPlants() {
-        for (int i = 0; i < 7; i++) {
-            plants.addAll(speciesMap.get(i).getPlants());
-        }
-        return plants;
-    }
+public List<Plant> getAllPlants() {
+    return speciesMap.stream()
+            .flatMap(sm -> sm.getPlants().stream())
+            .toList(); // Java 16+
+}
 
     // global change
     public void removeSpecies(Species species) {
